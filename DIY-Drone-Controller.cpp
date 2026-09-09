9b42ab5387fee2b75fa6a2a97039f89d110ba692
OjasAgrawall <ojas.e.agrawal@gmail.com>
2026-09-08 21:44:31 -0400

    it actually works

--- DIY-Drone-Controller.cpp
+++ DIY-Drone-Controller.cpp
@@ -17,7 +17,6 @@
 #define FLUSH_RX      0xE2
 #define W_TX_PAYLOAD  0xA0
 #define FLUSH_TX      0xE1
-#define NRF_ACTIVATE  0x50 // Send with 0x73 to activate or deactivate NRF_FEATURES Register
 #define R_RX_PL_WID   0x60
 
 // Core nRF24L01 Registers
@@ -33,11 +32,11 @@
 #define TX_ADDR       0x10 // 39A | A - TX Address for Auto Ack set RX_ADDR_P0 equal to this value | Reset value = 0xE7E7E7E7E7
 #define RX_PW_P0      0x11 // xxAAAAAA | A - # of Bytes to expect in RX payload in data pipe 0 | Useless when using dynamic payload width
 #define DYNPD         0x1C // xxABCDEF | A - F Enable dynamic payload width on Pipe 5 - 0 | REQUIRES EN_DPL and ENAA_P#
-#define NRF_FEATURES  0x1D // xxxxxABC | A - Enables Dynamic Payload width (EN_DPL) | B - Enables Payload with ACK | C - Enables the W_TX_PAYLOAD_NOACK Command | REQUIRES The ACTIVATE SPI COMMAND to be activated
-#define FIFO_STATUS   0x17 // Testing idk
+#define NRF_FEATURES  0x1D // xxxxxABC | A - Enables Dynamic Payload width (EN_DPL) | B - Enables Payload with ACK | C - Enables the W_TX_PAYLOAD_NOACK Command
+#define FIFO_STATUS   0x17 // xABCxxDE | A - TX_REUSE | B - TX is Full | C - TX is empty | D - RX is full | E - RX is empty
 //other values
-#define CHANNEL 76
-#define DATA_RATE 0x0F 
+#define CHANNEL 120
+#define DATA_RATE 0x26
 #define DATA_SIZE 0x03
 #define CONFIG_TX 0x02
 #define CONFIG_RX 0x03
@@ -53,16 +52,20 @@
 
 void nrf_send_cmd(uint8_t cmd){
     gpio_put(PIN_CSn, 0);
+    sleep_us(1);
     spi_write_blocking(NRF_SPI_PORT, &cmd, 1);
     gpio_put(PIN_CSn, 1);
+    sleep_us(1);
 }
 
 void nrf_write_reg(uint8_t reg, uint8_t value){
     uint8_t buffer[2] = { (W_REGISTER | reg), value};
 
     gpio_put(PIN_CSn, 0);
+    sleep_us(1);
     spi_write_blocking(NRF_SPI_PORT, buffer, 2);
     gpio_put(PIN_CSn, 1);
+    sleep_us(1);
 }
 
 void nrf_writeAlot_reg(uint8_t reg, uint8_t *buf, uint8_t len){
@@ -70,9 +73,11 @@ void nrf_writeAlot_reg(uint8_t reg, uint8_t *buf, uint8_t len){
     uint8_t cmd = W_REGISTER | reg;
     
     gpio_put(PIN_CSn, 0);
+    sleep_us(1);
     spi_write_blocking(NRF_SPI_PORT, &cmd, 1); 
     spi_write_blocking(NRF_SPI_PORT, buf, len);
     gpio_put(PIN_CSn, 1);
+    sleep_us(1);
 }
 
 uint8_t nrf_read_reg(uint8_t reg) {
@@ -82,8 +87,10 @@ uint8_t nrf_read_reg(uint8_t reg) {
 
     // This transmits buffer and fills rx_buf at the exact same time.
     gpio_put(PIN_CSn, 0);
+    sleep_us(1);
     spi_write_read_blocking(NRF_SPI_PORT, buffer, data, 2);
     gpio_put(PIN_CSn, 1);
+    sleep_us(1);
 
     // rx_buf[0] contains the nRF Status byte
     // rx_buf[1] contains the actual register value we asked for
@@ -95,30 +102,31 @@ uint8_t nrf_get_status(){
     uint8_t status;
 
     gpio_put(PIN_CSn, 0);
+    sleep_us(1);
     spi_write_read_blocking(NRF_SPI_PORT, &NO_OP, &status, 1);
     gpio_put(PIN_CSn, 1);
+    sleep_us(1);
 
     return status;
 }
 
 
-void nrf_send_data(uint8_t data[DATA_SIZE]){
-    uint8_t tx_buffer[DATA_SIZE + 1];
+void nrf_send_data(uint8_t data_size, uint8_t data[]){
+    uint8_t tx_buffer[data_size + 1];
     tx_buffer[0] = W_TX_PAYLOAD;
     uint8_t status = 0;
-    gpio_put(PIN_CE, 0);
 
-
-    
-    for (int i = 0; i < DATA_SIZE; i++) {
+    for (int i = 0; i < data_size; i++) {
         tx_buffer[i + 1] = data[i];
     }
 
-    printf("FIFO_STATUS befoire payload = %02X\n", nrf_read_reg(FIFO_STATUS));
+    printf("FIFO_STATUS before payload = %02X\n", nrf_read_reg(FIFO_STATUS));
 
     gpio_put(PIN_CSn, 0);
-    spi_write_blocking(NRF_SPI_PORT, tx_buffer, DATA_SIZE + 1);
+    sleep_us(1);
+    spi_write_blocking(NRF_SPI_PORT, tx_buffer, data_size + 1);
     gpio_put(PIN_CSn, 1);
+    sleep_us(1);
 
     printf("FIFO_STATUS after payload = %02X\n", nrf_read_reg(FIFO_STATUS));
 
@@ -131,21 +139,18 @@ void nrf_send_data(uint8_t data[DATA_SIZE]){
 
     printf("After CE FIFO = %02X\n", nrf_read_reg(FIFO_STATUS));
 
-
-
     //read status flags rq
     int timeout = 100; // Prevent infinite loop if hardware detaches
     while (timeout > 0) {
-
         status = nrf_get_status();
         if ((status & 0x30) != 0) { // Check if either MAX_RT (0x10) or TX_DS (0x20) is set
             timeout = 0;
         }
-        sleep_us(100);
+        sleep_ms(2);
         timeout--;
     }
-
-
+    status = nrf_get_status();
+    printf("After timeout %02X\n", status);
 
     if ((status & 0x10) == 0x10){
         printf("message not sent :(\n");
@@ -157,15 +162,14 @@ void nrf_send_data(uint8_t data[DATA_SIZE]){
     // reset status flags and flush the transmission buffer
     nrf_write_reg(NRF_STATUS, 0x70); 
     nrf_send_cmd(FLUSH_TX);
-
 }
 
-uint8_t message[3] = {0x10, 0x20, 0x30};
+uint8_t message[5] = {0x10, 0x20, 0x30, 0x40, 0x43};
 
 int main(){
     stdio_init_all();
 
-        // SPI initialisation. This example will use SPI at 1MHz.
+    // SPI 1MHz.
     spi_init(NRF_SPI_PORT, 1000*1000);
     gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
     gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
@@ -180,6 +184,7 @@ int main(){
 
     //CSn active low when sending message
     gpio_put(PIN_CSn, 1);
+sleep_us(1);
 
     //init nrf
     nrf_write_reg(CONFIG, CONFIG_TX);
@@ -191,15 +196,15 @@ int main(){
     // nrf_write_reg(RX_PW_P0, DATA_SIZE);
     nrf_write_reg(EN_RXADDR, 0x01);
     nrf_write_reg(SETUP_AW, 0x03);
-    nrf_write_reg(SETUP_RETR, 0x2F);
+    nrf_write_reg(SETUP_RETR, 0x0F);
 
-    uint8_t nrf_addr[5] = {0xE7, 0xE7, 0xE7, 0xE7, 0xE7};
-    nrf_writeAlot_reg(RX_ADDR_P0, nrf_addr, 5);
-    nrf_writeAlot_reg(TX_ADDR, nrf_addr, 5);
+    uint8_t nrf_address_1[5] = {0xE7, 0xE7, 0xE7, 0xE7, 0xE7};
+    uint8_t nrf_address_2[5] = {0xC2, 0xC2, 0xC2, 0xC2, 0xC2};
+    nrf_writeAlot_reg(RX_ADDR_P0, nrf_address_1, 5);
+    nrf_writeAlot_reg(TX_ADDR, nrf_address_1, 5);
 
-    nrf_write_reg(NRF_ACTIVATE, 0x73);
+    nrf_write_reg(NRF_FEATURES, 0x04);
     nrf_write_reg(DYNPD, 0x01);
-    nrf_write_reg(NRF_FEATURES, 0x06);
 
     sleep_ms(5000);
 
@@ -234,71 +239,69 @@ int main(){
     gpio_pull_up(PIN_DOWN);
 
     while (true) {
+        // adc_select_input(CHANNEL_FB);
+        // uint16_t raw_FB = adc_read();
 
-
-        adc_select_input(CHANNEL_FB);
-        uint16_t raw_FB = adc_read();
-
-        adc_select_input(CHANNEL_LR);
-        uint16_t raw_LR = adc_read();
+        // adc_select_input(CHANNEL_LR);
+        // uint16_t raw_LR = adc_read();
         
-        adc_select_input(CHANNEL_YAWLR);
-        uint16_t raw_YAWLR = adc_read();
+        // adc_select_input(CHANNEL_YAWLR);
+        // uint16_t raw_YAWLR = adc_read();
 
-        bool up_pressed = !gpio_get(PIN_UP);
-        bool down_pressed = !gpio_get(PIN_DOWN);
-        bool running = gpio_get(PIN_SS_INPUT);
+        // bool up_pressed = !gpio_get(PIN_UP);
+        // bool down_pressed = !gpio_get(PIN_DOWN);
+        // bool running = gpio_get(PIN_SS_INPUT);
 
-        // printf("raw values:%d, %d, %d\n", raw_FB, raw_LR, raw_YAWLR);
+        // // printf("raw values:%d, %d, %d\n", raw_FB, raw_LR, raw_YAWLR);
 
-        if (running){
-            if(up_pressed){
-                printf("Up: %d\n", up_pressed);
-            }
+        // if (running){
+        //     if(up_pressed){
+        //         printf("Up: %d\n", up_pressed);
+        //     }
 
-            else if(down_pressed){
-                printf("Down: %d\n", down_pressed);
-            }
+        //     else if(down_pressed){
+        //         printf("Down: %d\n", down_pressed);
+        //     }
 
-            if(raw_FB > 2100){
-                printf("Forward\n");
-            }
+        //     if(raw_FB > 2100){
+        //         printf("Forward\n");
+        //     }
             
-            else if(raw_FB < 1950){
-                printf("Backward\n");
-            }
+        //     else if(raw_FB < 1950){
+        //         printf("Backward\n");
+        //     }
 
-            if(raw_LR > 2100){
-                printf("Right\n");
-            }
+        //     if(raw_LR > 2100){
+        //         printf("Right\n");
+        //     }
             
-            else if(raw_LR < 1950){
-                printf("Left\n");
-            }
+        //     else if(raw_LR < 1950){
+        //         printf("Left\n");
+        //     }
 
-            if(raw_YAWLR > 2150){ // stickdrift ig
-                printf("Turn Right\n");
-            }
+        //     if(raw_YAWLR > 2150){ // stickdrift ig
+        //         printf("Turn Right\n");
+        //     }
             
-            else if(raw_YAWLR < 1950){
-                printf("Turn Left\n");
-            }
-        }
-        else{
-            printf("off\n");
-        }
+        //     else if(raw_YAWLR < 1950){
+        //         printf("Turn Left\n");
+        //     }
+        // }
+        // else{
+        //     printf("off\n");
+        // }
 
-        sleep_ms(5); 
+        // sleep_ms(5); 
 
-        // printf("running\n");
+        printf("running %d\n", sizeof(message));
 
-        // uint8_t status = nrf_get_status();
-        // printf("STATUS: %02X\n", status);
+        uint8_t status = nrf_get_status();
+        printf("STATUS: %02X\n", status);
 
-        // nrf_send_data(message);
+        nrf_send_data(sizeof(message), message);
 
-        // // Wait 1 second before the next reading
-        // sleep_ms(1000);
+        // Wait 1 second before the next reading
+        sleep_ms(1000);
 
     }
 }
